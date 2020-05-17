c8fd0ff250a1fab80054aafb8c394ef2c7093ee1
Seojin Kim <1106laura@naver.com>
2020-05-18 00:31:02 +0900

    ~#12.4 room_detail CBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
 from django.utils import timezone
-from django.urls import reverse
-from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 # Create your views here.
@@ -18,9 +16,6 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+class RoomDetail(DetailView):
+
+    model = models.Room
