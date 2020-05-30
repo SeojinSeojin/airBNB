0903f047f6be6b86f2f838283483fd603df96d12
Seojin Kim <1106laura@naver.com>
2020-05-30 23:58:46 +0900

    ~#17.4 Github Login

--- users/models.py
+++ users/models.py
@@ -37,6 +37,13 @@ class User(AbstractUser):
         (CURRENCY_KRW, "krw"),
     )
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = ((LOGIN_EMAIL, "Email"), (LOGIN_GITHUB,
+                                              "Github"), (LOGIN_KAKAO, "Kakao"),)
+
     avatar = models.ImageField(upload_to="avatars", blank=True)
     gender = models.CharField(choices=GENDER_CHOICES,
                               max_length=10, blank=True)
@@ -48,8 +55,11 @@ class User(AbstractUser):
         choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
 
-    email_verifid = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=20, default="", blank=True)
+
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
 
     def verify_email(self):
         if self.email_email_verified is False:
