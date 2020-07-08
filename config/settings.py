babc26274cf540d06e56b525426826f2adc27ddd
SeojinSeojin <1106laura@naver.com>
2020-07-08 17:30:34 +0900

    ~24.4 : calendar

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,7 @@ PROJECT_APPS = [
     "core.apps.CoreConfig", "users.apps.UsersConfig", "rooms.apps.RoomsConfig", "reviews.apps.ReviewsConfig", "reservations.apps.ReservationsConfig", "lists.apps.ListsConfig", "conversations.apps.ConversationsConfig",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
